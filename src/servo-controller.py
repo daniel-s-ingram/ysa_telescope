14e823cbbba0d3293c6643cfddbcb6b4c8d6a689
daniel-s-ingram <ingramds@appstate.edu>
2017-12-04 13:37:05 -0500

    Update servo-controller.py

--- src/servo-controller.py
+++ src/servo-controller.py
@@ -9,8 +9,8 @@ pi = pigpio.pi()
 def callback(data):
 	roll = data.data[0]
         pitch = data.data[1]
-	roll_servo = int(555.6 * angle + 50000)
-	pitch_servo = int(555.6 * otherangle + 50000)	
+	roll_servo = int(555.6 * roll + 50000)
+	pitch_servo = int(555.6 * pitch + 50000)	
 
 	pi.hardware_PWM(12, 50, roll_servo)
         pi.hardware_PWM(13, 50, pitch_servo)
